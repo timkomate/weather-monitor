b456830d6435ff8c45053e3772029c2304a235c8
Mate Timko <timko.mate@gmail.com>
2023-08-11 14:17:56 +0100

    init commit

---
+++ weather_monitor.py
@@ -0,0 +1,42 @@
+import requests
+import time
+import json
+
+class WeatherMonitor:
+    def __init__(self, secrets_path, config_path):
+        self.secrets = self.load_json(secrets_path)
+        self.config = self.load_json(config_path)
+
+    @staticmethod
+    def load_json(file_path):
+        with open(file_path, "r") as json_file:
+            data = json.load(json_file)
+        return data
+
+    def fetch_weather_data(self):
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={self.config['city']}&units=metric&APPID={self.secrets['API_KEY']}"
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Raise exception for non-2xx status codes
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred while fetching weather data: {e}")
+            return None
+
+    def display_weather_info(self, weather_data):
+        if weather_data:
+            temp = weather_data["main"]["temp"]
+            humidity = weather_data["main"]["humidity"]
+            pressure = weather_data["main"]["pressure"]
+            print(f"Temperature: {temp}°C, Humidity: {humidity}%, Pressure: {pressure} hPa")
+
+    def run(self):
+        while True:
+            weather_data = self.fetch_weather_data()
+            if weather_data:
+                self.display_weather_info(weather_data)
+            time.sleep(self.config["dt"])
+
+if __name__ == "__main__":
+    monitor = WeatherMonitor("./secrets.json", "./config.json")
+    monitor.run()
